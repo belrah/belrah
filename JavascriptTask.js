b7e31775b65f727931b27b350590428d45ead9f6
Belrah <belrahj3@gmail.com>
2020-03-26 19:56:55 +0100

    Update JavascriptTask.js

--- JavascriptTask.js
+++ JavascriptTask.js
@@ -1,7 +1,7 @@
 
 let name ="Kalu Mercy";
-let course = ["HTML", "CSS", "JAVASCRIPT"];
-console.log("My name is; "+ name +" and i'm taking these courses;"+ course);
+let course = ["HTML",  "CSS",  "JAVASCRIPT"];
+console.log("My name is; "+ name +" and i'm taking these course:"+ course);
 
 for(let i=0; i<=200; i++){
   if(i%2===1) console.log(i)
